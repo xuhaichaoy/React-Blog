496f7b1bebbfb0bc8a22ca5e87dc37d1292db423
xuhaichaoy <xuhaichaoy@163.com>
2019-07-11 17:19:00 +0800

    数据库

--- app/src/models/Users.ts
+++ app/src/models/Users.ts
@@ -4,15 +4,15 @@ const UserModel = obj.sequelize.define('user', {
     // id: { type: obj.Sequelize.INTEGER, autoIncrement: true, primaryKeys: true, unique: true },
     // username: { type: obj.Sequelize.STRING, allowNull: false },
     // password: { type: obj.Sequelize.STRING, allowNull: false },
-    id: {
-        type: obj.Sequelize.INTEGER(11),
-        primaryKey: true,            // 主键
-        autoIncrement: true,         // 自动递增
-    },
-    username: obj.Sequelize.STRING(100),
-    password: obj.Sequelize.STRING(100),
-    createdAt: obj.Sequelize.BIGINT,
-    updatedAt: obj.Sequelize.BIGINT,
+    // id: {
+    //     type: obj.Sequelize.INTEGER(11),
+    //     primaryKey: true,            // 主键
+    //     autoIncrement: true,         // 自动递增
+    // },
+    // username: obj.Sequelize.STRING(100),
+    // password: obj.Sequelize.STRING(100),
+    // createdAt: obj.Sequelize.BIGINT,
+    // updatedAt: obj.Sequelize.BIGINT,
 }, {
     timestamps: false
 })
