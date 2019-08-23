cd60dbd85378be04f7e0b8b8fbd5596dc7163d41
xuhaichaoy <xuhaichaoy@163.com>
2019-08-23 16:00:22 +0800

    chayi

--- app/dist/models/Users.js
+++ app/dist/models/Users.js
@@ -8,22 +8,29 @@ var UserModel = mysql_1.default.sequelize.define('user', {
     // id: { type: obj.Sequelize.INTEGER, autoIncrement: true, primaryKeys: true, unique: true },
     // username: { type: obj.Sequelize.STRING, allowNull: false },
     // password: { type: obj.Sequelize.STRING, allowNull: false },
-    id: {
+    uid: {
         type: mysql_1.default.Sequelize.INTEGER(11),
         primaryKey: true,
         autoIncrement: true,
     },
-    username: mysql_1.default.Sequelize.STRING(100),
-    password: mysql_1.default.Sequelize.STRING(100),
-    createdAt: mysql_1.default.Sequelize.BIGINT,
-    updatedAt: mysql_1.default.Sequelize.BIGINT,
+    nickName: mysql_1.default.Sequelize.STRING(100),
+    userName: mysql_1.default.Sequelize.STRING(100),
+    passWord: mysql_1.default.Sequelize.BIGINT,
+    Info: mysql_1.default.Sequelize.BIGINT,
+    Github: mysql_1.default.Sequelize.BIGINT,
+    image: mysql_1.default.Sequelize.BIGINT,
+    Date: mysql_1.default.Sequelize.BIGINT,
+    admin: mysql_1.default.Sequelize.BIGINT,
 }, {
     timestamps: false
 });
-UserModel.sync().on('success', function () {
-    console.log('aa..');
-}).on('failure', function () {
-    console.log('bb..');
+UserModel
+    .sync()
+    .then(function () {
+    console.log('table初始化完成！');
+})
+    .catch(function () {
+    console.log('init db error');
 });
 exports.default = UserModel;
 //# sourceMappingURL=Users.js.map
--- app/src/models/Users.ts
+++ app/src/models/Users.ts
@@ -4,24 +4,32 @@ const UserModel = obj.sequelize.define('user', {
     // id: { type: obj.Sequelize.INTEGER, autoIncrement: true, primaryKeys: true, unique: true },
     // username: { type: obj.Sequelize.STRING, allowNull: false },
     // password: { type: obj.Sequelize.STRING, allowNull: false },
-    // id: {
-    //     type: obj.Sequelize.INTEGER(11),
-    //     primaryKey: true,            // 主键
-    //     autoIncrement: true,         // 自动递增
-    // },
-    // username: obj.Sequelize.STRING(100),
-    // password: obj.Sequelize.STRING(100),
-    // createdAt: obj.Sequelize.BIGINT,
-    // updatedAt: obj.Sequelize.BIGINT,
+    uid: {
+        type: obj.Sequelize.INTEGER(11),
+        primaryKey: true,            // 主键
+        autoIncrement: true,         // 自动递增
+    },
+    nickName: obj.Sequelize.STRING(100),
+    userName: obj.Sequelize.STRING(100),
+    passWord: obj.Sequelize.BIGINT,
+    Info: obj.Sequelize.BIGINT,
+    Github: obj.Sequelize.BIGINT,
+    image: obj.Sequelize.BIGINT,
+    Date: obj.Sequelize.BIGINT,
+    admin: obj.Sequelize.BIGINT,
 }, {
     timestamps: false
 })
 
-UserModel.sync().on('success', function () {
-    console.log('aa..');
-}).on('failure', function () {
-    console.log('bb..');
-});
+UserModel
+  .sync()
+  .then(() => {
+    console.log('table初始化完成！')
+  })
+  .catch(() => {
+    console.log('init db error')
+  })
+
 export default UserModel
 
 
