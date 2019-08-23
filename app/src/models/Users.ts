9c2cbb93c6005c2f62a28c20690f6258496546e9
xuhaichaoy <xuhaichaoy@163.com>
2019-08-23 16:08:01 +0800

    同步

--- app/src/models/Users.ts
+++ app/src/models/Users.ts
@@ -1,11 +1,5 @@
 import obj from '../config/mysql'
 const UserModel = obj.sequelize.define('user', {
-<<<<<<< HEAD
-    // id: { type: obj.Sequelize.INTEGER, autoIncrement: true, primaryKeys: true, unique: true },
-    // username: { type: obj.Sequelize.STRING, allowNull: false },
-    // password: { type: obj.Sequelize.STRING, allowNull: false },
-=======
->>>>>>> 0a2842895a050d4b95061f6eb8e0d13a1fff931d
     uid: {
         type: obj.Sequelize.INTEGER(11),
         primaryKey: true,            // 主键
@@ -13,17 +7,10 @@ const UserModel = obj.sequelize.define('user', {
     },
     nickName: obj.Sequelize.STRING(100),
     userName: obj.Sequelize.STRING(100),
-<<<<<<< HEAD
-    passWord: obj.Sequelize.BIGINT,
-    Info: obj.Sequelize.BIGINT,
-    Github: obj.Sequelize.BIGINT,
-    image: obj.Sequelize.BIGINT,
-=======
     passWord: obj.Sequelize.STRING(100),
     Info: obj.Sequelize.STRING(100),
     Github: obj.Sequelize.STRING(100),
     image: obj.Sequelize.STRING(100),
->>>>>>> 0a2842895a050d4b95061f6eb8e0d13a1fff931d
     Date: obj.Sequelize.BIGINT,
     admin: obj.Sequelize.BIGINT,
 }, {
@@ -55,18 +42,6 @@ UserModel.fetch = async function () {
 };
 
 
-<<<<<<< HEAD
-UserModel
-  .sync()
-  .then(() => {
-    console.log('table初始化完成！')
-  })
-  .catch(() => {
-    console.log('init db error')
-  })
-
-=======
->>>>>>> 0a2842895a050d4b95061f6eb8e0d13a1fff931d
 export default UserModel
 
 
